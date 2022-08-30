fb240c9fb87abb70d5f161a752d5b450c35722e8
Berra Ozgun <berra@MSI_Admin1.mps.k12>
2022-08-30 13:01:50 -0700

    hi

---
+++ js-basics/index.js
@@ -0,0 +1,25 @@
+ // This is my first JavaScript code!
+ console.log('Hello World');
+ 
+ 
+ // Cannot be a reserved keyword
+ // Should be meaningful
+ // Cannot start with a number (1name)
+ // Cannot contain a space or hyphen (-)
+ // Are case-sensitive
+ 
+let firstName1 = 'Berra';
+let lastName = 'Ozgun';
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let name = 'Berra'; // String Literal
+let age = 14; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName2 = undefined;
+let selectedColor = null;
+
+console.log(name, age, isApproved, firstName2, selectedColor);
+
