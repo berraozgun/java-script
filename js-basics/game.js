11afbb8d970ff2acdedaf72e87429da405aa0ed6
Berra Ozgun <berraozgun7@gmail.com>
2022-09-20 13:00:22 -0700

    hi

---
+++ js-basics/game.js
@@ -0,0 +1,8 @@
+const secretNumber= Math.floor(Math.random() * 100);
+
+let userGuess=prompt("Please guess the number");
+
+while (number != secretNumber) {
+    let number = prompt ("Guess teh number");
+    if (number )
+}
