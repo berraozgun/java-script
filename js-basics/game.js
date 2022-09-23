89950f9a4deee4c0d23c352ef9526d968ad705b7
Berra Ozgun <berraozgun7@gmail.com>
2022-09-23 12:53:18 -0700

    hi

--- js-basics/game.js
+++ js-basics/game.js
@@ -3,22 +3,30 @@ let userGuess=null
 let trial=0
 
 while (userGuess !== secretNumber){
-userGuess=prompt("guess the number")
-trial+=1;
-alert("guess number: "+ trial)
-if (userGuess<secretNumber){
-    alert("Go up man!")
-}
+    userGuess=prompt("guess the number")
+    trial++;
+    alert("guess number: "+ trial)
 
-else if (userGuess>secretNumber){
-    alert("Go DOWN SIR!")
-}
+    if (userGuess<secretNumber){
+        alert("Go up!")
+    }
 
-else if(userGuess==secretNumber){
-    alert("congrats! you WON!")
-    break;
-}
-else {
-    alert("Stop playing around! Enter a valid number!!")
-}
+    else if (userGuess>secretNumber){
+        alert("Go DOWN!")
+    }
+
+    else if(userGuess==secretNumber){
+        alert("congrats! you WON!")
+        break;
+    }
+    else {
+        alert("Stop playing around! Enter a valid number!!")
+
+    }
+
+    if(trial>4){
+        alert ("Game over. The number was " + secretNumber)
+        break;
+    }
+
 }
